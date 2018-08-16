f0868bba30f25fdd96b0c5e7afe900b1588926de
JMEChristian <jmechristian@gmail.com>
2018-08-15 20:50:19 -0500

    Finished Config User Auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://admin:cowboy45@ds121262.mlab.com:21262/dev-connector"
+  mongoURI: "mongodb://admin:cowboy45@ds121262.mlab.com:21262/dev-connector",
+  secretKey: "secret"
 };

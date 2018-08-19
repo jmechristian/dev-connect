dae6bc28f2db8a7461be47c52d7527f2e9e6ab92
JMEChristian <jmechristian@gmail.com>
2018-08-19 10:54:54 -0500

    Finished Profile Routes

--- validation/register.js
+++ validation/register.js
@@ -23,7 +23,7 @@ module.exports = function validateRegisterInput(data) {
 
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required";
-  }
+  }                         
 
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required";

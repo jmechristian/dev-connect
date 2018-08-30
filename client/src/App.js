19e452ef27dffc7b5127fcc438c8ef1a4a400625
JMEChristian <jmechristian@gmail.com>
2018-08-29 20:50:12 -0500

    Added Create/Delete Exp

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,8 @@ import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/add-experience";
+import AddEducation from "./components/add-credentials/add-education";
 
 import PrivateRoute from "./components/Common/PrivateRoute";
 
@@ -64,6 +66,20 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
             </div>
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -39,6 +39,50 @@ export const createProfile = (profileData, history) => dispatch => {
     });
 };
 
+// Add Experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Education
+export const addEducation = (expEdu, history) => dispatch => {
+  axios
+    .post("/api/profile/education", expEdu)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete Experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This can NOT be undone!")) {

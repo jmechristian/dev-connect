daa0732825945ee05b1a8be9a9fbc9e9da6e4b6b
JMEChristian <jmechristian@gmail.com>
2018-09-03 19:58:58 -0500

    hope this works

--- server.js
+++ server.js
@@ -10,33 +10,29 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
-// Body Parser Middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//DB Config
+// DB Config
 const db = require("./config/keys").mongoURI;
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-// Passport Middleware
+// Passport middleware
 app.use(passport.initialize());
 
 // Passport Config
-require("./config/passport.js")(passport);
+require("./config/passport")(passport);
 
 // Use Routes
-
 app.use("/api/users", users);
-app.use("/api/posts", posts);
 app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
@@ -49,6 +45,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server Running on port ${port}`);
-});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));

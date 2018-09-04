6132a601fda71f55c065167e87263d917b050802
JMEChristian <jmechristian@gmail.com>
2018-09-03 20:16:04 -0500

    blah

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
-import TextFieldGroup from "../../Common/TextFieldGroup";
+import TextFieldGroup from "../Common/TextFieldGroup";
 
 class Login extends Component {
   state = {
